346fe248f71f15ab636927099fd64412acb09257
dudu0506 <dudu@mask.io>
2024-05-23 20:07:24 +0800

    feat: poll card ui

--- app/types.ts
+++ app/types.ts
@@ -18,4 +18,12 @@ export type ComposedPoll = Pick<Poll, 'id' | 'title' | 'created_at' | 'validInDa
     [key: `votes${number}`]: number;
 };
 
+export type PollTheme = {
+    titleColor: string;
+    optionBgColor: string;
+    optionTextColor: string;
+    optionSelectedBgColor: string;
+    cardBgColor: string;
+};
+
 export const POLL_EXPIRY = 60 * 60 * 24 * 180; // Expire polls after 3 months
---
+++ helpers/calculatePoll.ts
@@ -0,0 +1,19 @@
+import { ComposedPoll } from "@/app/types";
+
+export const calculatePoll = (poll: ComposedPoll) => {
+    const options = Object.keys(poll)
+        .filter((key) => key.startsWith("option"))
+        .map((key) => poll[key as `option${number}`]);
+    const totalVotes = options.reduce((acc, _, index) => {
+        return acc + Number(poll[`votes${index + 1}`]);
+    }, 0)
+
+    return options.map((option, index) => {
+        const votes = poll[`votes${index + 1}`];
+        const percentOfTotal = totalVotes
+            ? Math.round((votes / totalVotes) * 100)
+            : 0;
+
+        return { option, votes, percentOfTotal }
+    })
+}
