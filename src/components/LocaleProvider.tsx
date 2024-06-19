66dc2570ea627747091f703aa631150feece4f65
dudu0506 <dudu0506@users.noreply.github.com>
2024-06-19 09:49:25 +0000

    chore: prettier

--- src/components/LocaleProvider.tsx
+++ src/components/LocaleProvider.tsx
@@ -6,7 +6,7 @@ import { LOCALE } from '@/constants/enum';
 import { LocaleContext } from '@/hooks/useTranslate';
 
 export function LocaleProvider({ children }: { children: React.ReactNode }) {
-  const initialValue = useMemo(() => ({ locale: LOCALE.ZH }), []);
-  
-  return <LocaleContext.Provider value={initialValue}>{children}</LocaleContext.Provider>;
+    const initialValue = useMemo(() => ({ locale: LOCALE.ZH }), []);
+
+    return <LocaleContext.Provider value={initialValue}>{children}</LocaleContext.Provider>;
 }
